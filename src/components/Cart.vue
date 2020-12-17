13787c14a094bc93a34f877553ff4ec0df7aabd7
gitamgdtaula <gitamgadtaula@gmail.com>
2020-12-17 08:58:20 +0545

    cart logic completed

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>Cart</div>
+  <div>
+    <div class="item" v-for="(item, index) in cartItems" :key="index">
+      <a-row type="flex" justify="center">
+        <a-col>
+          <p>{{ item.name }}</p>
+          <p>
+            {{ item.country }}
+          </p>
+          <button @click="removeItem(item.id)">Remove</button>
+        </a-col>
+        <a-col
+          ><img
+            class="item-img"
+            :src="require(`@/assets/products/${item.image}`)"
+          />
+        </a-col>
+      </a-row>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    removeItem(id) {
+      this.$store.commit("removeFromCart", id);
+    },
+  },
+  computed: {
+    cartItems() {
+      return this.$store.state.items;
+    },
+  },
+};
 </script>
 
 <style>
